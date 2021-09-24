e3494a1439318d4f351e5a4dd46461a923669adc
Matteo Campini <matteo.campini@abcservicesrl.com>
2021-09-24 12:30:18 +0200

    Aggiornata applicazione ASP.NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ namespace KampsProg
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
