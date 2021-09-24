6011867857374265c4a937a617b25e5b460aaaf3
Matteo Campini <matteo.campini@abcservicesrl.com>
2021-09-24 09:52:07 +0200

    Primo Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace KampsProg
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            string firstargument = args.FirstOrDefault();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
